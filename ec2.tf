dd80699cd725fda310015f947f26a79a889c7730
Prasathanx <76684815+Prasathanx@users.noreply.github.com>
2022-09-09 13:00:24 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -17,7 +17,7 @@ data "aws_ami" "dep" {
 }
 
 resource "aws_instance" "New_Poc" {
-    ami = data.aws_ami.dep.id
+    ami = "ami-05fa00d4c63e32376"
     instance_type = "t2.micro"
     tags = {
         Name = "New_Poc_Data"
