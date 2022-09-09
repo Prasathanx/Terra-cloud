d849f73852a2f460a75a2765b237d672cc784426
Prasathanx <76684815+Prasathanx@users.noreply.github.com>
2022-09-09 13:01:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,3 @@
-data "aws_ami" "dep" {
-    most_recent = true
-    owners = [ "amazon"]
-    filter {
-      name = "name"
-      values = ["amzn2-ami-*"]
-    }
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
 resource "aws_instance" "New_Poc" {
     ami = "ami-05fa00d4c63e32376"
     instance_type = "t2.micro"
